d6234b3f204c6b2097bb5393934e9c76b80429fa
nidhi kumari <nk0619086@gmail.com>
2024-07-07 22:42:29 +0530

    another commit

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
-
+const cors = require('cors');
 const app = express();
 
 // Connect to database
 connectDB();
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 
 // Routes
@@ -16,3 +17,6 @@ app.use('/api/auth', authRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
